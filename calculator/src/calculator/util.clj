f3e6763a34fe5e7a4fb05f0b589a3d85a85a82d1
Geoff Tipton <geoffrey.tipton@kroger.com>
2014-04-07 13:12:24 -0400

    Things we did over the last 90 minutes.

--- calculator/src/calculator/util.clj
+++ calculator/src/calculator/util.clj
@@ -9,6 +9,22 @@
     (io/slurp-resource filename)
     (md/md-to-html-string)))
 (defn formulaPrinter [operator number1 number2]
-
 	(clojure.string/join " " [number1 operator number2 "="])
 	)
+
+(defn do-math [number1 operation number2]
+	(try 
+	((resolve (symbol operation)) number1 number2)
+	(catch ArithmeticException e "undefined")
+	)
+  )
+
+(defn parse-equation [equation]
+	(let [val (clojure.string/split equation #"/s")] 
+		(conj (read-string (first val)) (nth val 1) (read-string (last val)))
+	 )
+)
+
+; (defn solve-equation [equation]
+; 	(do-math (clojure.string/split equation))
+; 	)
